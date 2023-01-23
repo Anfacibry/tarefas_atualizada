cbfcdc5f662b857d7a4f1e32075de0789560287c
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-01-23 15:32:07 -0300

    atualizando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tarefas/theme/layout/cores.dart';
+
+import 'screens/home_tarefa.dart';
 
 void main() {
   runApp(const Tarefas());
@@ -16,54 +19,9 @@ class Tarefas extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
         useMaterial3: true,
+        scaffoldBackgroundColor: Cores.corFundo,
       ),
       home: const HomeTarefas(titulo: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomeTarefas extends StatelessWidget {
-  final String titulo;
-  const HomeTarefas({required this.titulo, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final double altura = MediaQuery.of(context).size.height;
-    final double largura = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Lista de tarefas",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: SizedBox(
-        height: altura,
-        width: largura,
-        child: Padding(
-          padding: EdgeInsets.all(largura * .1),
-          child: Column(
-            children: const [
-              Spacer(),
-              Text(
-                "Lista vazia",
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              Spacer(),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
