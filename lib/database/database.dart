56b5be9dc4e30b4778bfadb25a7b466be14288a8
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-02-10 14:56:42 -0300

    criando um banco de dados

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,16 +1,44 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:tarefas/models/tarefa.dart';
+
+String tabelaTarefa = "tarefa";
 
 Future<Database> database() async {
-  final Future<Database> db = openDatabase(
+  final Future<Database> bancoDeDados = openDatabase(
     join(await getDatabasesPath(), 'tarefa.db'),
     onCreate: (db, version) {
       return db.execute(
-        'CREATE TABLE tarefa(id INTEGER PRIMARY KEY, tarefa TEXT, data TEXT)',
+        'CREATE TABLE $tabelaTarefa(id INTEGER PRIMARY KEY, tarefa TEXT, data TEXT)',
       );
     },
     version: 1,
   );
 
-  return db;
+  return bancoDeDados;
+}
+
+Future<void> inserindoTarefa(Tarefa tarefa) async {
+  final Database bancoDeDados = await database();
+  await bancoDeDados.insert(
+    tabelaTarefa,
+    tarefa.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<List<Tarefa>> listaTarefas() async {
+  final Database bancoDeDados = await database();
+
+  final List<Map<String, dynamic>> maps =
+      await bancoDeDados.query(tabelaTarefa);
+
+  return List.generate(
+    maps.length,
+    (index) => Tarefa(
+      id: maps[index]["id"],
+      tarefa: maps[index]["tarefa"],
+      data: maps[index]["data"],
+    ),
+  );
 }
