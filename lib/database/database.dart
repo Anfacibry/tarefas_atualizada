a96eff97e47e8c3b152851b950984a3d9b9059be
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-02-27 10:46:55 -0300

    atualizando banco de dados

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,18 +1,21 @@
+import 'package:flutter/material.dart';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:tarefas/models/tarefa.dart';
 
-String tabelaTarefa = "tarefa";
+String tarefaDb = "tarefa";
+String tabelaTarefa = "tabelaTarefa";
 String id = "id";
-String tituloTarefa = "tituloTarefa";
+String titulo = "titulo";
 String data = "data";
 
 Future<Database> database() async {
   final Future<Database> bancoDeDados = openDatabase(
-    join(await getDatabasesPath(), '$tabelaTarefa.db'),
+    join(await getDatabasesPath(), '$tarefaDb.db'),
     onCreate: (db, version) {
       return db.execute(
-        'CREATE TABLE $tabelaTarefa($id INTEGER PRIMARY KEY, $tituloTarefa TEXT, $data TEXT)',
+        'CREATE TABLE $tabelaTarefa($id INTEGER PRIMARY KEY, $titulo TEXT, $data TEXT)',
       );
     },
     version: 1,
@@ -22,26 +25,44 @@ Future<Database> database() async {
 }
 
 Future<void> inserindoTarefa(Tarefa tarefa) async {
-  final Database bancoDeDados = await database();
-  await bancoDeDados.insert(
-    tabelaTarefa,
-    tarefa.toMap(),
-    conflictAlgorithm: ConflictAlgorithm.replace,
-  );
+  try {
+    final Database bancoDeDados = await database();
+    await bancoDeDados.insert(
+      tabelaTarefa,
+      tarefa.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    debugPrint("Pegando data: ${tarefa.data}");
+  } catch (erro) {
+    debugPrint(erro.toString());
+    return;
+  }
+}
+
+Future<void> excluindoBd() async {
+  Database db = await database();
+  await db.delete(tabelaTarefa);
 }
 
 Future<List<Tarefa>> listaTarefas() async {
-  final Database bancoDeDados = await database();
+  try {
+    final Database bancoDeDados = await database();
 
-  final List<Map<String, dynamic>> maps =
-      await bancoDeDados.query(tabelaTarefa);
-
-  return List.generate(
-    maps.length,
-    (index) => Tarefa(
-      id: maps[index][id],
-      tarefa: maps[index][tituloTarefa],
-      data: maps[index][data],
-    ),
-  );
+    final List<Map<String, dynamic>> maps =
+        await bancoDeDados.query(tabelaTarefa);
+    debugPrint(maps.toString());
+    debugPrint("Data: ${maps[0][data].runtimeType.toString()}");
+    debugPrint("Titulo: ${maps[0][titulo].runtimeType.toString()}");
+    return List.generate(
+      maps.length,
+      (index) => Tarefa(
+        id: maps[index][id],
+        tarefa: maps[index][titulo],
+        data: DateTime.parse(maps[index][data]),
+      ),
+    );
+  } catch (erro) {
+    debugPrint(erro.toString());
+    return List.empty();
+  }
 }
