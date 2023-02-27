ce5fb02e3a93706b420a3740a7e8cd7c81a3ab86
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-02-27 10:48:58 -0300

    atualizando main e pubspec para receber as atualização e os pacotes adicionais

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:tarefas/theme/layout/cores.dart';
 
 import 'screens/home_tarefa.dart';
@@ -13,9 +15,15 @@ class AppTarefa extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // excluindoBd();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale("pt", "BR")],
       theme: ThemeData(
           useMaterial3: true,
           scaffoldBackgroundColor: Cores.corFundo,
