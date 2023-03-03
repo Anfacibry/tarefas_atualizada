3a58462edb5b94e8be57394cef0ccd96fdfc7045
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-03-03 16:57:40 -0300

    Adicionando uma função booleana que irá verificar se a tarefa estará como concluida ou não

--- lib/models/tarefa.dart
+++ lib/models/tarefa.dart
@@ -2,19 +2,22 @@ class Tarefa {
   int id;
   String tarefa;
   DateTime data;
+  bool isFeito;
   Tarefa({
     required this.id,
     required this.tarefa,
     required this.data,
+    required this.isFeito,
   });
 
   Map<String, dynamic> toMap() => {
         "id": id,
         "titulo": tarefa,
         "data": data.toIso8601String(),
+        "feito": isFeito == true ? 1 : 0,
       };
   @override
   String toString() {
-    return "Id pego: $id, tarefa: $tarefa, data: ${data.toString()}";
+    return "Id pego: $id, tarefa: $tarefa, data: ${data.toString()}, feito: $isFeito";
   }
 }
