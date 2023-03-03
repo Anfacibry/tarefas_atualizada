8b9ad53f253e0d01a8d56050e86f37974432216e
Anfacibry <jogos.fabriciocintra@gmail.com>
2023-03-03 16:56:37 -0300

    Adicionando funções no banco de dados: Atualizar tarefa, confirmar tarefa e excluir tarefa

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -9,13 +9,19 @@ String tabelaTarefa = "tabelaTarefa";
 String id = "id";
 String titulo = "titulo";
 String data = "data";
+String isFeito = "feito";
 
 Future<Database> database() async {
   final Future<Database> bancoDeDados = openDatabase(
     join(await getDatabasesPath(), '$tarefaDb.db'),
     onCreate: (db, version) {
       return db.execute(
-        'CREATE TABLE $tabelaTarefa($id INTEGER PRIMARY KEY, $titulo TEXT, $data TEXT)',
+        '''CREATE TABLE $tabelaTarefa(
+          $id INTEGER PRIMARY KEY, 
+          $titulo TEXT, 
+          $data TEXT, 
+          $isFeito INTEGER
+          )''',
       );
     },
     version: 1,
@@ -32,16 +38,32 @@ Future<void> inserindoTarefa(Tarefa tarefa) async {
       tarefa.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
-    debugPrint("Pegando data: ${tarefa.data}");
+    debugPrint("Pegando data: ${tarefa.toMap()}");
   } catch (erro) {
-    debugPrint(erro.toString());
+    debugPrint("Erro pego: ${erro.toString()}");
+    debugPrint("Verificando: ${tarefa.toMap()}");
     return;
   }
 }
 
-Future<void> excluindoBd() async {
+Future<void> excluindoTarefa(int id) async {
   Database db = await database();
-  await db.delete(tabelaTarefa);
+  await db.delete(
+    tabelaTarefa,
+    where: "id = ?",
+    whereArgs: [id],
+  );
+}
+
+Future<void> atualizandoTarefa(Tarefa tarefa) async {
+  final db = await database();
+
+  db.update(
+    tabelaTarefa,
+    tarefa.toMap(),
+    where: "id = ?",
+    whereArgs: [tarefa.id],
+  );
 }
 
 Future<List<Tarefa>> listaTarefas() async {
@@ -50,19 +72,17 @@ Future<List<Tarefa>> listaTarefas() async {
 
     final List<Map<String, dynamic>> maps =
         await bancoDeDados.query(tabelaTarefa);
-    debugPrint(maps.toString());
-    debugPrint("Data: ${maps[0][data].runtimeType.toString()}");
-    debugPrint("Titulo: ${maps[0][titulo].runtimeType.toString()}");
+    debugPrint("Lista pega da Lista de Tarefas: ${maps.toString()}");
     return List.generate(
       maps.length,
       (index) => Tarefa(
         id: maps[index][id],
         tarefa: maps[index][titulo],
         data: DateTime.parse(maps[index][data]),
+        isFeito: maps[index][isFeito] == 1,
       ),
     );
   } catch (erro) {
-    debugPrint(erro.toString());
     return List.empty();
   }
 }
